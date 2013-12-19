4851dc862f6fbdc19556e0c5df66978cc004c50b
ivanoliveri <ipoliveri@gmail.com>
2013-12-19 01:15:36 -0300

    Primera integracion con Instagram

--- Stalktter/Controllers/HomeController.cs
+++ Stalktter/Controllers/HomeController.cs
@@ -5,6 +5,12 @@ using System.Web;
 using System.Web.Mvc;
 using LinqToTwitter;
 using System.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using System.Collections;
 
 namespace Stalktter.Controllers
 {
@@ -47,6 +53,27 @@ namespace Stalktter.Controllers
                 tweets = srch,
                 twitterProfilePicture = user.ProfileImageUrl
             };
+
+            WebClient client = new WebClient();
+
+            /*UGLY HACK*/
+            var userIDFromInstagram = client.DownloadString("http://jelled.com/instagram/user.php?username=" + startViewModel.instagramUsername);
+
+            dynamic userIDDeserialized = JsonConvert.DeserializeObject(userIDFromInstagram);
+
+            string userID = userIDDeserialized.data[0].id;
+
+            var instagramResponse = client.DownloadString("https://api.instagram.com/v1/users/" + userID + "/media/recent/?client_id=d837e1e08fe24802bfe7916b9ed9962c");
+
+            dynamic instagramResponseDeserialized = JsonConvert.DeserializeObject(instagramResponse);
+
+            var urlLowResolutions = new List<string>();
+
+            /*CONS 20*/
+            for (var i = 0; i < 20; i++)
+                urlLowResolutions.Add(instagramResponseDeserialized.data[i].images.low_resolution.url.ToString());
+
+            homeViewModel.instagramPhotoLinks = urlLowResolutions;
 
             return View(homeViewModel);
 
